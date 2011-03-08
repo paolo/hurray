216718860151e2197e8f4678d1477fcadf82e784
Paolo Castro <paolocastro.deb@gmail.com>
2011-03-08 00:47:13 -0400

    Creating the Window in the start method to make it more friendly with the Obj-C way of running the app.

--- test/helloworld/helloworld.rb
+++ test/helloworld/helloworld.rb
@@ -7,10 +7,11 @@ require "hrect"
 class HelloWorldController < HApplicationController
   
   def initialize
-    @view = HWindow.new HRect.new(0, 0, 320, 480)
   end
   
   def start
+    @view = HWindow.new HRect.new(0, 0, 320, 480)
+    
     label = HLabel.new HRect.new(20, 20, 150, 25)
     label.text = "Hello World"
     
